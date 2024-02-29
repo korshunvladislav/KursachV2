11ebf7170b5bd56bfbcf964ff4fe54ed85510a66
Vladislav <vladislav.korshun.2005@gmail.com>
2024-02-29 17:48:34 +0300

    Добавлены новые файлы и внесены изменения в существующие файлы

--- app.py
+++ app.py
@@ -9,26 +9,25 @@ points_data = []
 def index():
     return render_template('index.html')
 
-@app.route('/draw-graph')
-def draw_graph():
-    return render_template('draw_graph.html')
-
-@app.route('/input-points')
-def input_points():
-    return render_template('input_points.html')
-
 @app.route('/get_points', methods=['POST'])
 def get_points():
     data = request.get_json()
     shapes = data['shapes']
     points = []
+    points_for_txt = []
     for shape in shapes:
         if 'path' in shape:
             path = shape['path'].split('Z')[0]
             coords = path.split('L')[1:]
             for c in coords:
                 x, y = map(float, c.split(','))
-                points.append(({'x': x, 'y': y}))
+                points.append(({'x': "{:.4f}".format(round(x, 4)), 'y': "{:.4f}".format(round(y, 4))}))
+                print(points)
+                points_for_txt.append(("{:.4f}".format(round(x, 4)), "{:.4f}".format(round(y, 4))))
+    file = open("dots.txt", "w")
+    for x, y in points_for_txt:
+        file.write(f"{x} {y}\n")
+    file.close()
     return jsonify({'points': points})
 
 @app.route('/save_points', methods=['POST'])
@@ -39,20 +38,12 @@ def save_points():
         json.dump(points, f)
     return 'Точки успешно сохранены в файл points.json'
 
-@app.route('/submit_points', methods=['POST'])
-def submit_points():
-    data = request.form
-    points = []
-    
-    for key, value  in data.items():
-        if key.startswith('x'):
-            index = key[1:]
-            x = value
-            y = data['y' + index]
-            points.append({'x': x, 'y': y})
-    
-    with open('points.json', 'w') as f:
-        json.dump(points, f)
+@app.route('/plot_points')
+def plot_points():
+    with open("output_dots.txt", "r") as file:
+        points = [tuple(map(float, line.strip().split())) for line in file.readlines()]
+    return render_template('plot.html', points=json.dumps(points))
+
 
 if __name__ == '__main__':
     app.run(port='4999', debug=True)
--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,6 @@
 var pointsOutput = document.getElementById('output-container');
 var graphDiv = document.getElementById('function-graph');
 
-// Получение точек с нарисованного графика
 function getPoints() {
     var shapes = graphDiv.layout.shapes;
     fetch('/get_points', {
@@ -24,7 +23,6 @@ function getPoints() {
     .catch(error => console.error('Ошибка:', error));
 }
 
-// Сохранение точек в файл
 function savePoints(points) {
     fetch('/save_points', {
         method: 'POST',
@@ -38,58 +36,16 @@ function savePoints(points) {
     .catch(error => console.error('Ошибка:', error));
 }
 
-// Очистка графика
+
 function clearGraph() {
     var update = {shapes: []};
     Plotly.relayout(graphDiv, update);
     pointsOutput.innerHTML = '';
 }
 
-
-// function submitPoints() {
-//     var points = [];
-
-//     var xInput = document.getElementById('x').value.trim()
-//     var yInput = document.getElementById('y').value.trim()
-
-//     if (xInput && yInput) {
-//         var x = parseFloat(xInput);
-//         var y = parseFloat(yInput);
-
-//         var point = ( x: x, y: y );
-//         points.push(point)
-//     }
-// }
-
-// function clearPoints() {
-//     document.getElementById('points-output').value = '';
-//     document.getElementById('output-container').innerHTML = '';
-// }
-
-// Добавить поля ввода X и Y
-function addPoint() {
-    var pointsInputContainer = document.getElementById('points-input-container');
-    
-    var xInput = document.createElement('input');
-    xInput.type = 'text';
-    xInput.placeholder = 'Введите X';
-    xInput.name = 'x';
-    xInput.classList.add('input');
-
-    var yInput = document.createElement('input');
-    yInput.type = 'text';
-    yInput.placeholder = 'Введите Y';
-    yInput.name = 'y';
-    yInput.classList.add('input');
-    
-    pointsInputContainer.appendChild(document.createElement('br'));
-    pointsInputContainer.appendChild(xInput);
-    pointsInputContainer.appendChild(yInput);
-}
-
 Plotly.newPlot('function-graph', [{x: [], y: [], mode: 'lines', line: {color: 'blue'}}], {
-    title: 'Нарисуйте график функции',
-    xaxis: {title: 'X'},
-    yaxis: {title: 'Y'},
+    title: 'Нарисуйте свой график',
+    xaxis: {title: 'X', range: [-10, 10]},
+    yaxis: {title: 'Y', range: [-10, 10]},
     dragmode: 'drawopenpath'
 });
